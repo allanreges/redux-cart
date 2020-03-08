e519075c3b385f45736cd17b4598c870a6edf7b4
Allan de Carvalho <allan.reges@fbiz.com.br>
2020-03-08 11:30:17 -0300

    feature complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Router } from 'react-router-dom'
+import Routes from './routes'
+import GlobalStyle from './styles/global'
+import Header from './components/Header'
+import { Provider } from 'react-redux'
+import './config/reactotronConfig'
+import { ToastContainer } from 'react-toastify'
+import history from './services/history'
 
-function App() {
+import store from './store'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <Router history={history}>
+        <Header></Header>
+        <Routes>
+        </Routes>
+        <GlobalStyle></GlobalStyle>
+        <ToastContainer autoClose={2000}></ToastContainer>
+      </Router>
+    </Provider>
+  )
 }
 
-export default App;
